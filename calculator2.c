b0c2b164146fd6a62b8bfb71f7b55bf2d2a6b3c6
Farm <nm.fabr@gmail.com>
2026-02-04 00:59:20 +0700

    add some code to project-tier-ssr page

---
+++ calculator2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("*---------------------------------*") ;
+    printf("\n|      Project Calculator         |") ;
+    printf("\n*---------------------------------*") ;
+
+    //printf("\nStart Program Enter : 1");
+    //printf("\nEnd Program Enter : 2");
+
+    printf("\n-----------------") ;
+    printf("\n|               |") ;
+    printf("\n-----------------") ;
+    printf("\n| 7   8   9   / |") ;
+    printf("\n| 4   5   6   x |") ;
+    printf("\n| 1   2   3   - |") ;
+    printf("\n| 0   .   =   + |") ;
+
+
+    return 0 ;
+}
